df26668b9c2a45bc6e91df0c302a51d3f18249f2
Sven Hübner <sven.huebner.it@gmx.de>
2021-07-24 20:01:06 +0200

    add packaging and sample project that consumes the package

---
+++ SampleNuget/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SampleNuget
+{
+    public class Class1
+    {
+        public void Test()
+        {
+            var pocoInstance = new Mysamplenamespace.MyRootElement();
+        }
+    }
+}
